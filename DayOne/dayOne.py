cba95ee06006df711c6a0670ca663293608a14cd
Patryk Pasterny <patryk.pasterny@grafikoptymalny.pl>
2020-12-03 01:19:52 +0100

    first commit

---
+++ DayOne/dayOne.py
@@ -0,0 +1,16 @@
+def readfile(name):
+        with open(name) as myFile:
+            return myFile.read()
+
+readMyFile = readfile("C:/Users/admin/Desktop/MyImportant/AOC/DayOne/input.txt")
+nums = [int(x) for x in readMyFile.split('\n') if x.isnumeric()]
+
+tempNums = nums[:]
+for num in nums:
+    tempNums.remove(num)
+    for num2 in tempNums:
+        if (num + num2) == 2020:
+            print("1:" + str(num) + " " + "2:" + str(num2)) 
+            print("Result: " + str(num * num2))
+
+print("dupa")
